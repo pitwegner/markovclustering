5a5fcf3e75c92330175f860b85e27d621fba0452
pit.wegner <pit.wegner@zorc-cybernetics.com>
2019-10-14 17:21:10 +1100

    refactor 2 node clusters, change link material and change node colors

--- 3d-test/index.js
+++ 3d-test/index.js
@@ -1,22 +1,46 @@
-let clusterMaterial = {color: 0x00ff00, transparent: true, opacity: 0.4}
-let data = weight_clustered_data
+let clusterMaterial = {color: 0xffff00, transparent: true, opacity: 0.7}
+const url = new URL(window.location.href)
+const cluster = url.searchParams.get("cluster")
+let data
+if (cluster === 'weight') {
+  data = weight_clustered_data
+} else if (cluster === 'uniform') {
+  data = clustered_data
+} else {
+  data = clustered_data
+  data.clusters = []
+}
+
+nodeColors = {
+  'Director': 0x6baed6,
+  'CEO': 0x2171b5,
+  'Business Support Unit': 0x74c476,
+  'Business Control Unit': 0x9e9ac8,
+  'Business Development Unit': 0xfd8d3c,
+  'Business Support Manager': 0x238b45,
+  'Business Control Manager': 0x6a51a3,
+  'Business Development Manager': 0xd94701
+}
 
 const Graph = ForceGraph3D()
 	(document.getElementById("3d-graph"));
 Graph.resetProps();
 Graph.cooldownTicks(200)
   .nodeLabel('name')
-  .nodeAutoColorBy('position')
-  .linkWidth(1)
-  .linkMaterial((link) => {
-    if (data.clusters.filter((cluster) => {
-      return cluster.length === 2 && cluster[0] === link.source && cluster[1] === link.target
-    }).length > 0) return new THREE.MeshLambertMaterial(clusterMaterial)
-
-  })
-  .linkDirectionalParticleWidth(2)
-  .linkDirectionalParticles((link) => link.weight + 1)
-  .linkDirectionalParticleSpeed(0)
+  .nodeColor((node) => nodeColors[node.position_in_structure])
+  .nodeRelSize(3)
+  .linkWidth((link) => link.weight / 2)
+  .linkColor(0x999999)
+  .linkOpacity(1)
+  .nodeOpacity(1)
+  //.linkMaterial((link) => {
+  //  if (data.clusters.filter((cluster) => {
+  //    return cluster.length === 2 && cluster[0] === link.source && cluster[1] === link.target
+  //  }).length > 0) return new THREE.MeshLambertMaterial(clusterMaterial)
+  //})
+  //.linkDirectionalParticleWidth(1)
+  //.linkDirectionalParticles(2)
+  //.linkDirectionalParticleSpeed(0)
   .forceEngine('ngraph')
   .graphData(data)
   .onEngineStop(() => {
@@ -32,17 +56,44 @@ Graph.cooldownTicks(200)
       let geometry
       if (node_positions.length > 3) {
         geometry = new THREE.ConvexGeometry(node_positions)
+        const mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial(material))
+        scene.add(mesh)
       } else if(node_positions.length === 3) {
         geometry = new THREE.Geometry();
         geometry.vertices.push(...node_positions)
         geometry.faces.push(new THREE.Face3(0, 1, 2))
         material.side = THREE.DoubleSide
+        const mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial(material))
+        scene.add(mesh)
       } else if(node_positions.length === 2) {
-        // handled above in linkMaterial()
+
+        const r = 2.5;
+        const geometry = new THREE.CylinderBufferGeometry(r, r, 1, 10, 1, false);
+        geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 1 / 2, 0));
+        geometry.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI / 2));
+
+        const line = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial(material))
+
+        const start = node_positions[0]
+        const end = node_positions[1]
+        const vStart = new THREE.Vector3(start.x, start.y || 0, start.z || 0);
+        const vEnd = new THREE.Vector3(end.x, end.y || 0, end.z || 0);
+        const distance = vStart.distanceTo(vEnd);
+
+        line.position.x = vStart.x;
+        line.position.y = vStart.y;
+        line.position.z = vStart.z;
+
+        line.scale.z = distance;
+
+        scene.add(line);
+
+        line.parent.localToWorld(vEnd); // lookAt requires world coords
+        line.lookAt(vEnd);
+
+
       } else {
         // Dunno..? -> Doesn't occur
       }
-      const mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial(material))
-      scene.add(mesh)
     })
   })
